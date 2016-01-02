cf6c6cf0fd758faf8ca0bc972fc4c430d7f86933
Suwit <suwit.saiphan@hotmail.com>
2016-01-03 06:37:04 +0700

    Relocation user module from vendor to module folder

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -7,8 +7,6 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'phpDocumentor' => array($vendorDir . '/phpdocumentor/reflection-docblock/src'),
-    'ZfcUser' => array($vendorDir . '/zf-commons/zfc-user/src'),
-    'ZfcBase' => array($vendorDir . '/zf-commons/zfc-base/src'),
     'ZendXml\\' => array($vendorDir . '/zendframework/zendxml/library'),
     'Prophecy\\' => array($vendorDir . '/phpspec/prophecy/src'),
     'Doctrine\\ORM\\' => array($vendorDir . '/doctrine/orm/lib'),
@@ -20,6 +18,4 @@ return array(
     'Doctrine\\Common\\' => array($vendorDir . '/doctrine/common/lib'),
     'DoctrineORMModule\\' => array($vendorDir . '/doctrine/doctrine-orm-module/src'),
     'DoctrineModule\\' => array($vendorDir . '/doctrine/doctrine-module/src'),
-    'CsnUser\\' => array($vendorDir . '/coolcsn/csn-user/src'),
-    'CsnAuthorization\\' => array($vendorDir . '/coolcsn/csn-authorization/src'),
 );
